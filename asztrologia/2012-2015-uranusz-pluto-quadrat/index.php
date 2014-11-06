a4c994e690ddc49a19c3305180929a1ddeb78067
sorskapu <sorskapu@gmail.com>
2014-11-06 21:56:32 +0100

    Fix charset of 2012-2015-uranusz-pluto-quadrat/index.php

--- asztrologia/2012-2015-uranusz-pluto-quadrat/index.php
+++ asztrologia/2012-2015-uranusz-pluto-quadrat/index.php
@@ -3,7 +3,7 @@
 
 <head>
 
-    <meta charset="iso-8859-2">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="A Plútó és az Uránusz jelenleg derékszöget alkotnak egymással. Ez tehát az újjászületés időszaka.">
