850f0e411d3a40c0bfe5053904d27aa51ea7e84c
sorskapu <sorskapu@gmail.com>
2014-11-09 00:20:21 +0100

    update / kontakt

--- assets/inc/kontakt.php
+++ assets/inc/kontakt.php
@@ -3,17 +3,18 @@
                     <i class="fa fa-facebook"></i> <a href="https://www.facebook.com/sorskapu" title="Sorskapu hivatalos facebook oldala">facebook</a> | <i class="fa fa-youtube-play"></i> <a href="https://www.youtube.com/channel/UCw4fFvvNTVlj3hqsGH8uRbA" title="Sorskapu hivatalos youtube csatornája">youtube</a> | <i class="fa fa-vimeo-square"></i> <a href="http://vimeo.com/sorskapu" title="Sorskapu hivatalos vimeo csatornája">vimeo</a></p>
                     <br /><br />
                     
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-
 <!--AdSense-->
 <div class="col-md-4 col-md-offset-3">
-<!-- sorskapu -->
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- sorskapu 2 -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
+     style="display:inline-block;width:300px;height:250px"
      data-ad-client="ca-pub-8101132359371519"
-     data-ad-slot="5202794180"></ins>
+     data-ad-slot="8173156583"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
+
 </div>
 <!--/AdSense-->
