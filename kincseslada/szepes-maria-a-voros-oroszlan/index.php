ad461d04f660f2a90a1440662ccd12afb2db5e86
sorskapu <sorskapu@gmail.com>
2014-11-09 15:18:59 +0100

    update / a-voros-oroszlan

--- kincseslada/szepes-maria-a-voros-oroszlan/index.php
+++ kincseslada/szepes-maria-a-voros-oroszlan/index.php
@@ -90,234 +90,54 @@
             <div class="row">
                 <div class="col-lg-12">
 				
-                <!--1.51-->
+                <!--A Vörös Oroszlán-->
                 <div class="col-sm-6 col-md-4">
                     <div class="thumbnail">
                                                                       
                         <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/OEovStx2uwY?rel=0" frameborder="0" allowfullscreen></iframe>
+                        <iframe width="490" height="276" src="//www.youtube.com/embed/videoseries?list=PLsdXqNsQmlpkmeJDCJ0HHT0TehWX7C_Iu" frameborder="0" allowfullscreen></iframe>
                         </div>
+
+                        <div class="caption">
+                            <h4>A Vörös Oroszlán</h4>                            
+                        </div><!-- /.caption -->
 
                     </div><!-- /thumbnail -->
  				</div><!-- /col-sm-6 col-md-4 -->
-                <!--/1.51-->
+                <!--/A Vörös Oroszlán-->
 
-                <!--2.51-->
+                <!--Első könyv-->
                 <div class="col-sm-6 col-md-4">
                     <div class="thumbnail">
                                                                       
                         <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/Qxg-aDv5zHA?rel=0" frameborder="0" allowfullscreen></iframe>
+                        <iframe width="490" height="276" src="//www.youtube.com/embed/videoseries?list=PLsdXqNsQmlpm6uw5xC46pTraNOv53634A" frameborder="0" allowfullscreen></iframe>
                         </div>
+
+                        <div class="caption">
+                            <h4>A Vörös Oroszlán (Első könyv)</h4>                            
+                        </div><!-- /.caption -->
 
                     </div><!-- /thumbnail -->
                 </div><!-- /col-sm-6 col-md-4 -->
-                <!--/2.51-->
+                <!--/Első könyv-->
 
-                <!--3.51-->
+                <!--Második könyv-->
                 <div class="col-sm-6 col-md-4">
                     <div class="thumbnail">
                                                                       
                         <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/TmEbSBLqKzA?rel=0" frameborder="0" allowfullscreen></iframe>
+                        <iframe width="490" height="276" src="//www.youtube.com/embed/videoseries?list=PLsdXqNsQmlpnVhQOvuWpOwEjgGVVsC6Y_" frameborder="0" allowfullscreen></iframe>
                         </div>
+
+                        <div class="caption">
+                            <h4>A Vörös Oroszlán (Második könyv)</h4>                            
+                        </div><!-- /.caption -->
 
                     </div><!-- /thumbnail -->
                 </div><!-- /col-sm-6 col-md-4 -->
-                <!--/3.51-->
-
-                <!--4.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/RpNTEb7BREM?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/4.51-->
-
-                <!--5.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/vNhpzBD00Lw?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/5.51-->
-
-                <!--6.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/13q0tFCqP9s?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/6.51-->
-
-                <!--7.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/TFNrqvowkU0?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/7.51-->
-
-                <!--8.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/lMEsi0SvUwU?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/8.51-->
-
-                <!--9.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/bOPwA2hCD74?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/9.51-->
-
-                <!--10.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/15z6El_AkyA?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/10.51-->
-                
-                <!--11.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/XO3jiTFv4Vk?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/11.51-->
-
-                <!--12.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/abDsflDbyAM?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/12.51-->
-
-                <!--13.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/jPBYLXiA9d4?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/13.51-->
-
-                <!--14.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/7vdJaPSz-e8?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/14.51-->
-
-                <!--15.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/3Q7jC_cPTfw?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/15.51-->
-
-                <!--16.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/k1l0xTVaB38?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/16.51-->
-
-                <!--17.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/5T9ZX2ud-UA?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/17.51-->
-
-                <!--18.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/TccpBxEwSP8?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/18.51-->
-
-                <!--19.51-->
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                                                                      
-                        <div class="responsive-video">
-                        <iframe width="490" height="276" src="//www.youtube.com/embed/Wv_EOdbNnMk?rel=0" frameborder="0" allowfullscreen></iframe>
-                        </div>
-
-                    </div><!-- /thumbnail -->
-                </div><!-- /col-sm-6 col-md-4 -->
-                <!--/19.51-->
-
+                <!--/Második könyv-->
+               
             </div><!-- /row -->
         </div><!-- /container -->
 		
