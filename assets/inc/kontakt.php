0b0d8305e56a7f720edd49568d7de05a4f04662d
sorskapu <sorskapu@gmail.com>
2014-11-09 01:00:46 +0100

    update / kontakt

--- assets/inc/kontakt.php
+++ assets/inc/kontakt.php
@@ -1,4 +1,4 @@
-<h4><strong>Kapcsolat</strong></h4>
+<h4><strong>Elérhetőségeink</strong></h4>
 					<p><i class="fa fa-phone fa-fw"></i> <a href="tel:+36-30-655-3866" title="Hívjon bizalommal">(+3630) 655-3866</a> | <i class="fa fa-envelope-o fa-fw"></i> <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#111;&#114;&#115;&#107;&#97;&#112;&#117;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;" title="Email címünk">&#115;&#111;&#114;&#115;&#107;&#97;&#112;&#117;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</a></p>
                     <i class="fa fa-facebook"></i> <a href="https://www.facebook.com/sorskapu" title="Sorskapu hivatalos facebook oldala">facebook</a> | <i class="fa fa-youtube-play"></i> <a href="https://www.youtube.com/channel/UCw4fFvvNTVlj3hqsGH8uRbA" title="Sorskapu hivatalos youtube csatornája">youtube</a> | <i class="fa fa-vimeo-square"></i> <a href="http://vimeo.com/sorskapu" title="Sorskapu hivatalos vimeo csatornája">vimeo</a></p>
                     <br /><br />
