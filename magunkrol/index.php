3129929f97f60ca971fd92e06bee98c696e1dda4
sorskapu <sorskapu@gmail.com>
2014-11-09 16:42:40 +0100

    new page / magunkrol

---
+++ magunkrol/index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Bemutatkozas.">
+	<meta name="keywords" content="családfelállítás, családállítás, asztrológia, sámánizmus, mágia, sors, erő, Peter Orban, Bert Hellinger, Kerekes Andrea, Symbolon, morfogenetikus mező, szellemi mező, terápia, lélek,">
+    <link rel="shortcut icon" href="http://cdn.sorskapu.hu/sorskapu.ico">
+	
+	<!-- Facebook -->
+	<meta property="fb:app_id" content="1459644830989485" />
+    <meta property="fb:admins" content="100000956925477"/>
+    <meta property="og:url" content="http://www.sorskapu.hu/magunkrol/"/>
+    <meta property="og:locale" content="hu_HU" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Sorskapu" />
+	<meta property="og:title" content="Magunkról" />
+    <meta property="og:description" content="Bemutatkozás" />
+    <meta property="og:image" content="http://cdn.sorskapu.hu/fb_magunkrol.jpg" />
+    <!-- Facebook Like End -->
+
+    <title>Magunkról</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../assets/css/stylish-portfolio.css" rel="stylesheet">
+	
+    <!-- Custom Fonts -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300italic,400italic,600,600italic,700,700italic,900,900italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+   
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+	<!-- Contact Form -->
+		
+</head>
+<!--Google Analytics-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-18292409-16', 'sorskapu.hu');
+  ga('send', 'pageview');
+
+</script>
+<!--//Google Analytics-->
+<body>
+    <!-- Facebook Like -->
+    <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1459644830989485&version=v2.0";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+    </div>
+
+    <!--Menu-->
+	<?php include '../assets/inc/menu.php';?>
+	<!--/Menu-->
+
+    <!--Header-->
+    <header id="top" class="header_magunkrol">
+        <div class="text-vertical-center">
+            <h3 style="margin-top:100px;">Magunkról</h3>
+			<br />
+            <a href="#tovabb" class="btn btn-dark btn-lg guru">Tovább</a><br /><br />
+			<div class="fb-like" data-href="http://sorskapu.hu/magunkrol/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+		</div>
+	</header>
+	<!--Header-->
+
+    <!--Magunkról-->
+    <section id="tovabb" style="padding:50px 0;">
+	
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+				
+				    <div id="tovabb" class="responsive-video"><iframe width="1140" height="641" src="//www.youtube.com/embed/b5Dyi-jhjPY?rel=0" frameborder="0" allowfullscreen></iframe></div>
+				    					
+								
+					<div class="panel panel-default">
+                    <div class="panel-heading"><h2>Magunkról</h2></div>
+                    <div class="panel-body">
+                    <p class="lead" style="text-align:justify;">Szeretettel üdvözlünk mindenkit, aki megtisztel minket a figyelmével!</p>
+                    <p class="lead" style="text-align:justify;">Úgy gondoltuk, ennyi kedves látogató után már illő  bemutatkoznunk, ezért most magamról, magunkról, a Sorskapuról és az asztrológiáról fogok beszélni egy kicsit.</p>
+                    <p class="lead" style="text-align:justify;">A nevem Kerekes Andrea. Már több mint húsz éve foglalkozom asztrológiával, évekig tanultam kineziológiát, pszichológiát, mégsem tartom magam sem asztrológusnak, sem kineziológusnak, pszichológusnak pedig végképp nem, hiszen ebből a tárgyból végzettséget  sem szereztem. Célom soha nem az volt, hogy valamiféle szakmát tanuljak, miközben forgattam a jobbnál jobb e témákban íródott könyveket, hanem az, hogy minél inkább el tudjak igazodni saját kis világomban. Hogy negyven éves koromra mégis ennyire előtérbe került nálam az emberekkel való lelki munka, az a téma iránti elhivatottságomnak és sorsomnak köszönhető. Egyszerűen így alakult. Kinyílt előttem egy kapu, én pedig beléptem rajta.</p>
+                    <p class="lead" style="text-align:justify;">Kislány korom óta az emberek és a köztük létrejövő kapcsolatok érdekelnek a legjobban. Emlékszem, hetedikes voltam, amikor kaptunk egy új, fiatal magyartanárnőt, aki arra kért minket, hogy mutatkozzunk be és mondjuk el, mivel töltjük a szabadidőnket, mivel szeretünk foglalkozni. Amikor rám került a sor, szó szerint ezt mondtam: engem az emberek érdekelnek. Persze az egész osztály röhögni kezdett, viszont a tanárnő szívébe rögtön beloptam magam. Később azt mondta a szüleimnek, hogy sose látott még ilyen bátor gyereket. Akkor még nem igazán értettem, mi ebben a bátorság, ma már tudom.</p>
+                    <p class="lead" style="text-align:justify;">Aztán felnőttkoromra kiderült, hogy bizonyos szempontból valójában a gyáva emberek közé tartozom, és erről éppen az asztrológia vetette le a fátylat. Megtudtam ugyanis, hogy Mérleg az ascendensem. Azt pedig, hogy az osztálytársaim, majd később a munkatársaim gyakran kinevettek, csúfoltak és kiközösítettek, az ascendensemen álló Uránuszomnak köszönhetem. Ahogyan azt is, hogy fanatikus érdeklődésem ellenére képtelen voltam végigjárni egy négy éves asztrológiai iskolát. Megpróbáltam ugyan, de hamar rá kellett jönnöm, hogy az a rendszer, ami ott működik, nem nekem való. Én a saját tempómban szeretek haladni, ezért rendkívül zavart, hogy éveket kellett volna várnom, mire előadták volna nekem azokat a dolgokat, melyek a legjobban érdekeltek. Unatkoztam az órákon, ezért négy-öt alkalom után abba is hagytam az egészet. Hazasiettem és magam tanulmányoztam tovább - akkor éppen - Friedrich Schwickert csodálatos könyveit. A horoszkópok kiszámításában és megrajzolásában érzéki örömöt leltem, szerelmes voltam az asztrológiába, s ez a szerelem mind a mai napig él. Gondolom, ebből rájöttetek, hogy a Napom a Bika jegyében áll.</p>
+                    <p class="lead" style="text-align:justify;">Annak ellenére, amiket az előbb elmeséltem, számomra is elhozta az Isten a mestert, a nagy tanítót, Dr. Peter Orban személyében. Semmi kétségem nincs afelől, hogy kettőnk találkozása sorsszerű volt. Ahogyan váratlanul belépett az életembe, azt úgy éltem meg, mintha csoda történt volna. S hogy később lehetőségem nyílt arra, hogy személyesen is tanulhassak tőle, az egyszerűen minden elképzelésemet meghaladta. Fantasztikus ember ő, ami már írásain keresztül is teljesen átjött nekem, de az évek során meg is győződhettem arról, hogy ez valóban így van. Csoporttársaim közül nem mindenki volt ilyen elégedett vele, de én megtaláltam benne azt a tapasztalt, bölcs, öreg és örök barátot, akire mindig is vágytam. Hála Istennek, ez a barátság kölcsönös. Átsegített életem nehéz időszakain, miközben folyamatosan tanított, anélkül, hogy tanítani akart volna, s anélkül, hogy valaha is beleavatkozott volna az életembe. Támogatását mind a mai napig élvezem, és ezért rendkívül  hálás vagyok. Az elmúlt években létrehoztunk néhány közös munkát, s a jövőre nézve is tervezek még sok mindent, ami vele kapcsolatos. Idén töltötte be 70. életévét, Isten éltesse, remélem, sokáig velünk marad!</p>
+                    <p class="lead" style="text-align:justify;">Visszatérve az asztrológiára, szeretném elmondani, hogy én a bolygókat különféle szerepszemélyiségekként kezelem. Mindegyikük bennünk él, a lelkünkben. S hogy melyikük kerül előtérbe, illetve melyiket fojtjuk el, az egy horoszkópból azonnal kiderül. Épp úgy, ahogy az univerzumban, lelkünkben is folyamatosan mozog és változik minden. Én az asztrológiát ennek a változásnak az elősegítésére használom. A horoszkóp számomra isteni segédeszköz ahhoz, hogy kapcsolódni tudjak a hozzám forduló emberekhez. Ezen kívül megspórolja nekem az időt, mert egyetlen szimpla beszélgetés sem képes annyi mindenre rávilágítani, mint egy születési képlet. Egy horoszkóp megmutatja nekem a másik mélyen gyökerező félelmeit, megmutatja, miben tehetséges, elárulja, mire vágyik, milyen hajlamokkal jött a világra, milyen berögződéseket táplál, s mi az, ami leginkább hiányzik neki ahhoz, hogy boldog lehessen.</p>
+                    <p class="lead" style="text-align:justify;">Sokszor észre sem vesszük, hogy nem mi irányítjuk az életünket, hanem a tudatalattink. Ösztönösen reagálunk a dolgokra, személyiségünk alapvető jellemvonásain keresztül. Nem számolunk a következményekkel. Mindaddig, amíg a külső körülmények rá nem kényszerítenek minket. Most gondolj magadra! Ott állsz életed egy bizonyos pontján. A jelenben. Rád milyen kényszerítő erők hatnak? Mi az, ami gátol a fejlődésben, előrehaladásban? Életed melyik területén találkozol a legtöbb akadállyal? Mi az, amit nagyon akarsz, mégse sikerül?</p>
+                    <p class="lead" style="text-align:justify;">Sorolhatnám még a kérdéseket, de egy biztos: a végső válasz minden esetben a tudatalattidban rejlik. Ott rejtőzködik és arra vár, hogy észre vedd. Azt szeretné, hogy foglalkozz vele, törődj vele. Ha ezt megteszed, megjutalmaz téged: teljes valójában megmutatkozik előtted, és ezzel rendelkezésedre bocsátja azt az energiát, mellyel aztán sikerül továbblépned. Ez azt is jelenti, hogy a munkát minden esetben te végzed. Én csupán segítőd lehetek ezen az úton. Ha úgy érzed, elfáradtál, elakadtál, eltévedtél vagy egyszerűen csak szeretnéd jobban megismerni magad, én szívesen melléd szegődöm egy rövid időre, és legjobb tudásom szerint támogatlak téged. Megfogom a kezed és elkísérlek egy darabon, hogy aztán tágabb tudattal, nyugodtabb lélekkel és tisztább gondolatokkal mehess tovább a saját utadon.</p>
+                    <p class="lead" style="text-align:justify;">Fontos azt is megemlítenem, hogy mivel én Peter Orbantól nemcsak asztrológiát tanultam, hanem családállítást vezetni is, nálam ez a bizonyos hellingeri filozófia teljesen beépült a horoszkóp értelmezésének folyamatába. Amikor egy radixra nézek, azonnal szemet szúrnak a családi összefonódások, a régmúltból hozott minták, látom, hol vannak azok a területek, ahol minél előbb vissza kell állítani az egyensúlyt, létre kell hozni a harmóniát. Tulajdonképpen én ebben látom az asztrológia lényegét. Ahogy minden más dolog lényegét is ezen a világon. Az egyensúly, a harmónia, a nyugalom és a béke - ezek az iránymutatóim. Éppen ezért ezek létrejöttéért fáradozom, hűen Mérleg ascendensemhez. Nyilván az élet ad is ehhez kellő mennyiségű gyakorlópályát, ami azt jelenti, hogy magam is rengeteg nehézségen mentem már keresztül. Sorsom nem kímélt meg engem a különféle csapásoktól, a nehéz és kilátástalannak tűnő helyzetektől, s a szinte kibírhatatlanná duzzadt  konfliktusoktól. De tudom, hogy mindez nem véletlenül történt így. S azt is tudom már, hogy meg kellett tanulnom irányítani a sorsomat, már amennyire ez lehetséges. Elfogadni a dolgokat úgy, ahogy azok vannak, mindenképpen célravezető, sőt, mindig ez az első lépés. A következő pedig leginkább tudatosságomon múlik. Ezért nekem ennyi elég, jöhet bármi. Az élet számomra arról szól, hogy megtanuljam kezelni a múlt következményeit, azaz a jelen pillanatban megnyilvánuló helyzeteket.</p>
+                    <p class="lead" style="text-align:justify;">Az, hogy a sok nehézség ellenére sikerült megnyílnom a világ felé, páromnak köszönhető. Lelki életem alakulásának legintenzívebb élményeit vele élem át. S aztán vele beszélem meg azokat a lelki folyamatokat, melyeken keresztülmentem, s melyeken ő keresztülment. Sokat tanulunk egymástól, ahogy annak lennie kell. András a támaszom, aki segít nekem megvalósítani az álmaimat, s ezzel szerintem a legtöbbet adja, amit egy férfi adhat egy nőnek, akit szeret. Még azt is megteszi, ha kérem, hogy a hangját kölcsönzi a képzeletemben megjelenő szerepszemélyiségek ábrázolásához, ami egyáltalán nem egyszerű feladat. Mondom ezt 18 évnyi szinkronizálás után, hiszen ami a foglalkozásomat illeti, már jó ideje ezt csinálom. Sok-sok filmszerepben gyakorolhattam és gyakorolhatom, milyen az, mikor a hangom segítségével, a beszéden vagy az éneklésen keresztül kell megformálnom és átadnom különféle érzelmeket.</p>
+                    <p class="lead" style="text-align:justify;">Így alakult tehát, hogy 2011-ben megszületett a Sorskapu, melynek születése után három éven keresztül más feladata nem volt, minthogy növekedjen és erősödjön bennünk csendesen, s mely idén augusztusban végre saját útjára indult. Mind a ketten bízunk benne, hogy ez az út eredményes lesz, sikeres, változatos és lélekemelő, s bízunk abban is, hogy végigkísér minket az egész életen. Sőt, még azon is túl!</p>
+                    </div>
+                    </div>
+					
+										
+					<div class="well well-sm">
+					<h4><strong></strong>Kerekes Andrea</h4>
+					<i class="fa fa-info-circle fa-lg"></i> 
+					Végzettségeim: <a href="//cdn.sorskapu.hu/kerekes-andrea-csaladfelallito-oklevel.jpg">Családállítás</a> | <a href="//cdn.sorskapu.hu/kerekes-andrea-asztrologus-kepzes-oklevel.jpg">Asztrológia</a>
+					<center><img class="img-responsive" style="margin:21px auto 21px auto;" src="//cdn.sorskapu.hu/kerekes-andrea.jpg" /></center>
+					
+					</div>
+																			    
+				</div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+		
+    </section>
+	<!--/Magunkról-->
+     
+    <!--Footer-->
+	<section id="kapcsolat" style="padding:50px 0;">
+	
+    <footer>
+    <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                <div class="fb-like" data-href="http://sorskapu.hu/magunkrol/" data-layout="box_count" data-action="like" data-show-faces="true" data-share="true"></div>
+                <br /><br />    
+
+                <!--kontakt-->
+                <?php include '../assets/inc/kontakt.php';?>
+                <!--/kontakt-->
+				    
+					
+                    <hr class="small" style="margin: 50px auto 20px auto;">
+                    <p class="text-muted">Copyright &copy; sorskapu.hu 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+	
+	</section>
+	<!--/Footer-->
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	
+	<!-- Custom Theme JavaScript -->
+    <script>
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('.guru').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    </script>
+	
+</body>
+</html>
