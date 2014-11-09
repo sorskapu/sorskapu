3dc0ae15daeab65875f42154108ca03ff1665ef0
sorskapu <sorskapu@gmail.com>
2014-11-09 18:30:54 +0100

    update

--- csaladfelallitas/index.php
+++ csaladfelallitas/index.php
@@ -75,8 +75,8 @@
     <!--Header-->
     <header id="top" class="header_csaladfelallitas">
         <div class="text-vertical-center">
-            <a href="#tovabb" style="margin-top:100px;" class="btn btn-dark btn-lg">A családállításról</a><br /><br />
-            <a href="/csaladfelallitas/ujabb-gondolatok-a-temaban/" class="btn btn-dark btn-lg guru">Újabb gondolatok a témában</a><br /><br />
+            <a href="#tovabb" style="margin-top:100px;" class="btn btn-dark btn-lg guru">A családállításról</a><br /><br />
+            <a href="/csaladfelallitas/ujabb-gondolatok-a-temaban/" class="btn btn-dark btn-lg">Újabb gondolatok a témában</a><br /><br />
             <div class="fb-like" data-href="http://sorskapu.hu/csaladfelallitas/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
 		</div>
 	</header>
