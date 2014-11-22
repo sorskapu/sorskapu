45f4e3e76dc688294aea0ffa78e7115f733fd4c0
sorskapu <sorskapu@gmail.com>
2014-11-22 15:47:07 +0100

    update

--- assets/inc/menu.php
+++ assets/inc/menu.php
@@ -51,6 +51,7 @@
 				<li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Melléklet<b class="caret"></b></a>
                         <ul class="dropdown-menu">
+                        <li><a href="/melleklet/a-gondolat-teremto-hatalma/">A gondolat teremtő hatalma</a></li>
                             <li><a href="/melleklet/biztonsag/">Biztonság</a></li>
 						    <li><a href="/melleklet/kapcsolatok/">Kapcsolatok</a></li>
 							<li><a href="/melleklet/mert-most-tukor-altal-homalyosan-latunk/">Mert most tükör által homályosan látunk</a></li>
