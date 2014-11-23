bbccb1bc8f0f5d5b9cbdce94cac1faf5d54ace5d
sorskapu <sorskapu@gmail.com>
2014-11-23 15:01:28 +0100

    update

--- kincseslada/szepes-maria-a-voros-oroszlan/index.php
+++ kincseslada/szepes-maria-a-voros-oroszlan/index.php
@@ -205,7 +205,7 @@
 
                 <!--Bérlet-->
                 
-                        <p class="lead"><a href="http://cdn.sorskapu.hu/sorskapu-berlet.jpg" alt="Sorskapu Bérlet" title="Sorskapu Bérlet"><img class="img-responsive" style="margin:0 auto 0 auto;" width="600" src="http://cdn.sorskapu.hu/sorskapu-berlet.jpg" alt="Sorskapu Bérlet" title="Sorskapu Bérlet" /></a></p>
+                        <p class="lead"><a href="/berlet/" alt="Sorskapu Bérlet" title="Sorskapu Bérlet"><img class="img-responsive" style="margin:0 auto 0 auto;" width="600" src="http://cdn.sorskapu.hu/sorskapu-berlet.jpg" alt="Sorskapu Bérlet" title="Sorskapu Bérlet" /></a></p>
                         <p class="lead">Bérletünk szabadáras, azaz te magad határozhatod meg, hogy milyen összeggel támogatsz minket. Nagylelkűségeddel további terveink megvalósulását segíted elő. Hálánk jeléül különböző kedvezmények és meglepetések várnak rád!</p>
                         <p class="lead"><img class="img-responsive" style="margin:0 auto 0 auto;" src="http://cdn.sorskapu.hu/berlet-ganesha.jpg" alt="Sorskapu - Bérlet | Megálmodni, megteremteni és fenntartani!" /></p>
                         <p class="lead">Symbolon Kiadó Kft.<br />CIB Bank | 10701269-49382501-51100005<br /><b>Közlemény rovat:</b> Sorskapu Bérlet<br /><br />Számlázási adataidat a következő e-mail címre küldd: <br /><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#111;&#114;&#115;&#107;&#97;&#112;&#117;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;" title="Email címünk">&#115;&#111;&#114;&#115;&#107;&#97;&#112;&#117;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</a></p>
