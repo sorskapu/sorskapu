2f7c0c161c0b74c17cf151efe8751d26096a8c51
sorskapu <sorskapu@gmail.com>
2014-11-23 15:10:37 +0100

    update

--- kincseslada/szepes-maria-a-voros-oroszlan/index.php
+++ kincseslada/szepes-maria-a-voros-oroszlan/index.php
@@ -136,7 +136,7 @@
                     <p class="lead" style="text-align:justify;">Előre is köszönünk minden segítséget!</p>
                     <p class="lead" style="text-align:justify;">Kerekes Andrea és Ternyik András</p>
                     </div>
-                </div></div>
+                </div>
                 <!--/Előszó-->
 				
                 <!--A Vörös Oroszlán-->
@@ -203,6 +203,8 @@
                 </div><!-- /col-md-6 -->
                 <!--/Harmadik könyv-->
 
+                <div style="clear:both;"></div>
+
                 <!--Bérlet-->
                 <div class="jumbotron">
                     <p class="lead"><a href="/berlet/" alt="Sorskapu Bérlet" title="Sorskapu Bérlet"><img class="img-responsive" style="margin:0 auto 0 auto;" width="600" src="http://cdn.sorskapu.hu/sorskapu-berlet.jpg" alt="Sorskapu Bérlet" title="Sorskapu Bérlet" /></a></p>
@@ -212,7 +214,7 @@
                 </div>    
                 <!--/Bérlet-->
 
-            <!--</div><!-- /col-lg-12 -->
+            </div><!-- /col-lg-12 -->
             </div><!-- /row -->
         </div><!-- /container -->
 		
