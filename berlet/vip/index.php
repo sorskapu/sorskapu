09947258862c3ad302d2b8673dcca6e4ec222488
sorskapu <sorskapu@gmail.com>
2014-11-29 23:34:00 +0100

    update

--- berlet/vip/index.php
+++ berlet/vip/index.php
@@ -65,7 +65,7 @@
     <!--Header-->
     <header id="top" class="header_vip">
         <div class="text-vertical-center">
-            <h3 style="margin-top:100px;">Bérlettulajdonosok Oldala</h3>
+            <h3 style="margin-top:100px;">VIP Oldal</h3>
             <p>Kedves Bérlettulajdonos!<br /><br />Hálásan köszönjük támogatását, fogadja szeretettel ajándékunkat!</p>
 			<br />
             <a href="#" style="off margin-top:80px;" class="btn btn-dark btn-lg">Peter Orban - A hős utazása 1.</a>
