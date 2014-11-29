fc8a62205a44d3b8580b3fccefdd0d4bd2cc0192
sorskapu <sorskapu@gmail.com>
2014-11-29 23:28:44 +0100

    update

---
+++ berlet/vip/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Teremtsünk együtt!">
+	<meta name="keywords" content="bérlet, támogatás, adomány, hangoskönyv,">
+    <link rel="shortcut icon" href="http://cdn.sorskapu.hu/sorskapu.ico">
+	
+	<!-- Facebook -->
+	<meta property="fb:app_id" content="1459644830989485" />
+    <meta property="fb:admins" content="100000956925477"/>
+    <meta property="og:url" content="http://www.sorskapu.hu/berlet/vip/"/>
+    <meta property="og:locale" content="hu_HU" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Sorskapu" />
+	<meta property="og:title" content="Bérlet" />
+    <meta property="og:description" content="Teremtsünk együtt!" />
+    <meta property="og:image" content="http://cdn.sorskapu.hu/fb_berlet.jpg" />
+    <!-- Facebook Like End -->
+
+    <title>Bérlettulajdonosok Oldala</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../../assets/css/stylish-portfolio.css" rel="stylesheet">
+	
+    <!-- Custom Fonts -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300italic,400italic,600,600italic,700,700italic,900,900italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+   
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+	<!-- Contact Form -->
+		
+</head>
+<!--Google Analytics-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-18292409-16', 'sorskapu.hu');
+  ga('send', 'pageview');
+
+</script>
+<!--//Google Analytics-->
+<body>
+    
+    <!--Menu-->
+	<?php include '../../assets/inc/menu.php';?>
+	<!--/Menu-->
+
+    <!--Header-->
+    <header id="top" class="header_vip">
+        <div class="text-vertical-center">
+            <h3 style="margin-top:100px;">Bérlettulajdonosok Oldala</h3>
+            <p>Kedves Bérlettulajdonos!<br />Hálásan köszönjük a támogatását!<br />Fogadja szeretettel ajándékunkat!</p>
+			<br />
+            <a href="#" style="off margin-top:80px;" class="btn btn-dark btn-lg">Peter Orban - A hős utazása 1.</a>
+		</div>
+	</header>
+	<!--Header-->
+    
+    <!-- jQuery Version 1.11.0 -->
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	
+	<!-- Custom Theme JavaScript -->
+    <script>
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('.guru').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    </script>
+	
+</body>
+</html>
