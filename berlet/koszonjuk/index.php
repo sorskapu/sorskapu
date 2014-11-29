354f7cb5f2bf0f390ce61f48afb54abe905602a9
sorskapu <sorskapu@gmail.com>
2014-11-29 20:20:00 +0100

    update

--- berlet/koszonjuk/index.php
+++ berlet/koszonjuk/index.php
@@ -1,4 +1,4 @@
 <META http-equiv="refresh" content="5;URL=http://sorskapu.hu/berlet">
 <meta charset="utf-8">
 <br /><br /><br /><br /><br /><br /><br />
-<center>Köszönjük szépen a vásárlást!</center>
+<center><p style="font-family: "Source Sans Pro","Helvetica Neue",Helvetica,Arial,sans-serif;">Köszönjük szépen a vásárlást!<p></center>
