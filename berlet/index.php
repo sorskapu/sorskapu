f6d12717149886e10610e537b841019311478973
sorskapu <sorskapu@gmail.com>
2014-11-29 20:37:44 +0100

    update

--- berlet/index.php
+++ berlet/index.php
@@ -107,7 +107,7 @@
                         <input type="image" src="http://cdn.sorskapu.hu/paypal.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                         <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
                         </form>
-                        <br />
+                        <br /><br />
                         </div>
                         </div>
                     </div>
