8aded81c0e8e0dde802653c5adbb353a1fb86cc7
sorskapu <sorskapu@gmail.com>
2014-11-29 20:35:07 +0100

    update

--- berlet/index.php
+++ berlet/index.php
@@ -100,14 +100,14 @@
                         <div class="panel panel-default">
                         <div class="panel-heading"><h3>Bankkártyás fizetés</h3></div>
                         <div class="panel-body">
-                        
+                        <br />
                         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                         <input type="hidden" name="cmd" value="_s-xclick">
                         <input type="hidden" name="hosted_button_id" value="H2LJL2HBBMRJY">
                         <input type="image" src="http://cdn.sorskapu.hu/paypal.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                         <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
                         </form>
-                         
+                        <br />
                         </div>
                         </div>
                     </div>
