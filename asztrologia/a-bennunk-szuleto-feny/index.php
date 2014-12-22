0f1b45a5e542f2f990606e55956adac569eabcc9
sorskapu <sorskapu@gmail.com>
2014-12-22 23:11:40 +0100

    update

---
+++ asztrologia/a-bennunk-szuleto-feny/index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Öt bolygó a Bakban, sok-sok energia csoportosul egy helyen, egy időben. Gazdag és változatos élményekben lehet részünk.">
+	<meta name="keywords" content="újhold, holdfázisok, nap, hold, merkúr, vénusz, plútó, szaturnusz, bak, nyilas, együttállás, december 22, quadrát, a fény születése, téli napforduló, anima, animus, ellentétek, feszültség, asztrológia, symbolon, lélek, peter orban, családállítás, hellinger">
+    <link rel="shortcut icon" href="http://cdn.sorskapu.hu/sorskapu.ico">
+	
+	<!-- Facebook -->
+	<meta property="fb:app_id" content="1459644830989485" />
+    <meta property="fb:admins" content="100000956925477"/>
+    <meta property="og:url" content="http://www.sorskapu.hu/asztrologia/a-bennunk-szuleto-feny/"/>
+    <meta property="og:locale" content="hu_HU" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Sorskapu" />
+	<meta property="og:title" content="A bennünk születő fény" />
+    <meta property="og:description" content="Öt bolygó a Bakban, sok-sok energia csoportosul egy helyen, egy időben. Gazdag és változatos élményekben lehet részünk." />
+    <meta property="og:image" content="http://cdn.sorskapu.hu/fb_asztrologia-008.jpg" />
+    <!-- Facebook Like End -->
+ 
+    <title>A bennünk születő fény</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../../assets/css/stylish-portfolio.css" rel="stylesheet">
+	
+    <!-- Custom Fonts -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300italic,400italic,600,600italic,700,700italic,900,900italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+   
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+	<!-- Contact Form -->
+		
+</head>
+<!--Google Analytics-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-18292409-16', 'sorskapu.hu');
+  ga('send', 'pageview');
+
+</script>
+<!--//Google Analytics-->
+<body>
+    <!-- Facebook Like -->
+    <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1459644830989485&version=v2.0";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+    </div>
+
+    <!--Menu-->
+	<?php include '../../assets/inc/menu.php';?>
+	<!--/Menu-->
+
+    <!--Header -->
+    <header id="top" class="header_asztrologia-008">
+        <div class="text-vertical-center">
+            <h4 class="btn btn-dark btn-lg" style="margin-top:100px; margin-bottom:4px; word-wrap:break-word;"><a href="../#tovabb2">Asztrológiai írások</a> / A bennünk születő fény</h4>
+			<div style="clear: both;"></div>
+			<br />
+            <a href="#tovabb" class="btn btn-dark btn-lg guru">Tovább</a><br /><br />
+			<div class="fb-like" data-href="http://www.sorskapu.hu/asztrologia/a-bennunk-szuleto-feny/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+		</div>
+	</header>
+    <!--/Header -->
+	
+    <!--Asztrológia / A bennünk születő fény-->
+    <section id="tovabb" style="padding:50px 0;">
+	
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+				
+				    <div class="panel panel-default">
+					     <div class="panel-heading" style="text-align:center;">
+                         <h2>A bennünk születő fény</h2>
+                         <p style="margin:-10px auto 0 auto; font-size:18px;">2014. december 22.</p>
+                         </div>
+					     <div class="panel-body">
+						 <br /> 
+                         <p class="lead" style="text-align:justify;">2014. december 22-e több szempontból is különleges, ha az égbolton létrejövő változásokat figyeljük.</p>
+                         <p class="lead" style="text-align:justify;">Először is újhold van, tehát a Nap és a Hold együttállása figyelhető meg, ráadásul ez az együttállás egy jegyváltáskor történik, hiszen épp ma hajnalban léptek át a Nyilasból a Bakba. Nemrég a Vénusz, majd a Merkúr is a Bak jegyébe lépett, a Plútó pedig már évek óta ott tartózkodik.</p>
+                         <p class="lead" style="text-align:justify;">A másik, ami nagyon érdekes, hogy az Uránusz ma vet pontos quadrátot a Plútóra, ami együttáll a Vénusszal és a Merkúrral is, a Hold és a Nap pedig szintén efelé a quadrát felé közelít.</p>
+                         <p class="lead" style="text-align:justify;">Mindehhez társul a Szaturnusz jegyváltása, hiszen a Skorpióban töltött időszaka lejárt, 23-tól már a Nyilasban vándorol.</p>
+                         <p class="lead" style="text-align:justify;"><img class="img-responsive" style="off margin:21px auto 21px auto;" src="//cdn.sorskapu.hu/asztrologia-008-2.jpg" /></p>
+                         <p class="lead" style="text-align:justify;">Sok-sok energia csoportosul tehát egy helyen, egy időben. Gazdag és változatos élményekben lehet részünk. Nagy nyomás nehezedik ránk, gátakkal, akadályokkal találkozunk. Nemcsak a mindennapokkal kapcsolatos teendőink során érezhetjük blokkolva magunkat, hanem érzelmi és gondolati síkon is. Kommunikációnk nehézkessé válhat. Mélyről jövő gátlásaink megakadályozhatnak minket abban, hogy tisztán és érthetően fejezzük ki magunkat. Érezzük, hogy valami nagy megkönnyebbülésre vágyunk, szeretnénk feloldani magunkban a hűvös ész és az áradó érzelmek kettősségéből adódó feszültséget. Szeretnénk minden félelemtől megszabadulni, minden múltból eredő sebet begyógyítani, minden arcunkra fagyott könnycseppet felolvasztani.</p>
+                         <p class="lead" style="text-align:justify;">A földi lét és a látható világon túli birodalom között feszülő kötélen lépkedünk. A fizikai valóság terhe vállunkra nehezedve tol minket ide, a realitás keserű talajára, s közben húz a végtelen tér, melyben emlékeink szerint valamikor már otthonra találtunk. Igen, mindannyian tudjuk, hogy az, amit keresünk, nem itt található. Illetve itt is megtalálható, amennyiben létre tudjuk azt hozni. Nem egyszerű. Félelmetes, milyen emberek között vagyunk kénytelenek mozogni, miközben szorgalmasan végezzük hétköznapi kis feladatainkat. Egyre nehezebb elviselni azt a közeget, amit az utcára lépve szinte azonnal tapasztalunk. A szellemi korlátoltság gyakran megmutatkozik. Az érzelmi nyomor szintén. Tudatosságra kényszerít ez minket, akik szüntelenül kutatjuk értelmét ennek az egész értelmetlennek tűnő valaminek.</p>
+                         <p class="lead" style="text-align:justify;">Mit keresek én itt, ilyen alakok és ilyen körülmények között? Hol van a saját kis helyem ebben a rohanó, ám folyamatosan változó világban? Hogyan tudnék felszabadulni a rengeteg kényszer alól, amit a társadalom, a politika, a gazdaság vagy akár néhány szűk közösség rám erőszakol?</p>
+                         <p class="lead" style="text-align:justify;">Az én személyes válaszom az, hogy csakis belül találhatod meg a szabadságot, és csakis egy másik emberrel kapcsolatos élményen, tapasztaláson keresztül. A szabadság ugyanis nem más, mint az, amikor minden gátlás és akadály nélkül képes vagy átjutni a másik oldalra. Arra az oldalra, ahol könnyű vagy, mert egyetlen dologra tudsz csak figyelni: arra, hogy szeretsz. A végtelen tér, amiben feloldódsz egy-egy forrpontra hevült feszültség után, ugyanaz a hely, amit egy szoros, de gyengéd, meleg ölelésben találsz. Otthonra lelsz. Hirtelen átkerülsz abba a bizonyos másik világba.</p>
+                         <p class="lead" style="text-align:justify;">Ezért azt tudom csak mondani, hogy jelen helyzetünkben mi, emberek, annyit tehetünk, hogy önmagunkban és közeli kapcsolatainkban kezdjük el keresni a megoldást. Ne hagyjuk magunkat letörni más emberek által, ne meneküljünk a hétköznapi kötelességeink elől, ne zárkózzunk el, ne essünk depresszióba, hanem forduljunk azok felé, akik legszűkebb környezetünkben élnek, azokhoz, akik hozzánk tartoznak! Értsük meg, hogy semmi sem lesz könnyebb addig, amíg velük nem találjuk meg a hangot! Mindannyian ugyanarra vágyunk: figyelemre, megértésre, együttérzésre és nyugalomra! Legyünk nyitottak egymás érzései és gondolatai felé! Öleljük meg egymást, és adjuk át magunkat a szeretet szüntelen áramlásának! Minél többen tesszük ezt, annál kisebb lesz a nyomás társadalmi szinten is. Ne vesszünk el az alacsony erkölccsel rendelkező emberek között, és ne is foglalkozzunk velük! Figyelmünket azokra irányítsuk, akiknek szükségük van ránk! Azon legyünk, hogy bármelyik pillanatban képesek legyünk visszatalálni ahhoz az önmagunkhoz, aki nem ítélkezik, nem sajnálja magát, nem panaszkodik, csupán ad és szeret. Felelősségünk elsősorban abban áll, hogy mire figyelünk, mire összpontosítunk, mire gondolunk, azaz hogy mennyire vagyunk tudatosak, illetve mennyire irányít minket tudatalattink ösztönös, fájdalommal és indulatokkal terhes, sötét közege.</p>
+                         <p class="lead" style="text-align:justify;">Ezekben a napokban sokan kapjuk magunkat azon, hogy a halálon, az elmúláson gondolkodunk. Lehetnek halállal kapcsolatos élményeink, tapasztalataink is. De lehet, hogy elhunyt szeretteinkkel átélt emlékeink jönnek elő. Az Uránusz a halálfélelmünktől is szeretne most megszabadítani minket. Beleképzeltem magam a saját, eljövendő halálomba. Csodálatos volt. Óriási szeretet vett körül, s bennem is az volt. Így léptem át a határon, miközben a szeretet érzése egy folyamat volt, életen túl és halálon innen... Ez tette csodálatossá. És ez szüntette meg bennem a félelmet. Ekkor jöttem rá, hogy tulajdonképpen nem is a haláltól félünk, hanem attól, hogy hogyan halunk meg. Lesz-e bennünk elég szeretet, s lesz-e velünk valaki, aki átölel, s ezzel lehetővé teszi, hogy még itt, a Földön, a végtelen térben találjuk magunkat. Ez a hogyantól való félelem csak egyféleképpen oldható fel: arra kell rájönnünk és azt kell megtapasztalnunk, hogy hogyan kell élni! Hogyan kell úgy élni, mint aki éppen most haldoklik valaki szerető ölelésében!</p>
+                         <p class="lead" style="text-align:justify;">Mindenkinek áldott karácsonyt kívánunk! Szeretetben gazdag pillanatokat, felszabadulást a félelmek alól, megkönnyebbülést a terhek súlya alól! Igazán szoros öleléseket, megértő, gyengéd, elfogadó, szerető tekinteteket! Úgy ajándékozzuk meg egymást, mintha Jézus születésnapjára vinnénk ajándékot, személyesen neki, a Megváltónak! Mert mindannyiunknak vannak személyes megváltói, akik segítenek cipelni keresztünket, végtelen szeretettel, önzetlenül, s akiken keresztül végül mi is megválthatjuk saját magunkat.</p>
+                         <p><span style="color: grey;">Írta: Kerekes Andrea</span></p>
+                         
+						 </div>
+                    </div>
+																								    
+				</div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+		
+    </section>
+	<!--/Asztrológia / A bennünk születő fény-->
+	
+	<!-- FB Comments -->
+	    <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	                <div class="fb-comments" data-href="http://www.sorskapu.hu/asztrologia/a-bennunk-szuleto-feny/" data-width="100%" data-numposts="5" data-colorscheme="light"></div>
+	            </div>
+            </div>																					    
+	    </div>
+    <!-- /FB Comments -->
+     
+    <!--Footer-->
+	<section id="kapcsolat" style="padding:50px 0;">
+	
+    <footer>
+    <div id="kapcsolat" class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                <div class="fb-like" data-href="http://www.sorskapu.hu/asztrologia/a-bennunk-szuleto-feny/" data-layout="box_count" data-action="like" data-show-faces="true" data-share="true"></div>
+                <br /><br />
+
+                <!--kontakt-->
+                <?php include '../../assets/inc/kontakt.php';?>
+                <!--/kontakt-->
+					
+					      <hr class="small" style="margin: 50px auto 20px auto;">
+                <p class="text-muted">Copyright &copy; sorskapu.hu 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+	
+	</section>
+    <!--/Footer-->
+	
+    <!-- jQuery Version 1.11.0 -->
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	
+	<!-- Custom Theme JavaScript -->
+    <script>
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('.guru').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    </script>
+	
+</body>
+</html>
