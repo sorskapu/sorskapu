8827f71bb24c195350d98d35f967aac00a8c476e
sorskapu <sorskapu@gmail.com>
2015-01-27 15:41:31 +0100

    update

--- asztrologia/jelentkezes/index.php
+++ asztrologia/jelentkezes/index.php
@@ -77,13 +77,15 @@
 	
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
+                <div class="col-lg-12">
 				
+                <center>
 		        <div class="cognito">
                 <script src="https://services.cognitoforms.com/include/required"></script>
                 <script src="https://services.cognitoforms.com/session/script/100ca1c9-098e-4c44-ab18-5473ee7eafc5"></script>
                 <script>Cognito.load("forms", { id: "1" });</script>
                 </div>
+                </center>
 																								    
 				</div>
             </div>
