0d1f3a79cf9e9e55676f88aa6df4cd686810c9a2
sorskapu <sorskapu@gmail.com>
2015-02-22 19:31:50 +0100

    update

---
+++ hirlevel/index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Ha szeretnél figyelemmel kísérni minket, iratkozz fel hírlevelünkre.">
+	<meta name="keywords" content="sorskapu, hírlevél, aktuális események, családállítás, családfelállítás, Bert Hellinger, Peter Orban, Kerekes Andrea, csoportos terápia, lélek,">
+    <link rel="shortcut icon" href="http://cdn.sorskapu.hu/sorskapu.ico">
+	
+	<!-- Facebook -->
+	<meta property="fb:app_id" content="1459644830989485" />
+    <meta property="fb:admins" content="100000956925477"/>
+    <meta property="og:url" content="http://www.sorskapu.hu/hirlevel/"/>
+    <meta property="og:locale" content="hu_HU" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Sorskapu" />
+	<meta property="og:title" content="Sorskapu Hírlevél" />
+    <meta property="og:description" content="Ha szeretnél figyelemmel kísérni minket, iratkozz fel hírlevelünkre." />
+    <meta property="og:image" content="http://cdn.sorskapu.hu/fb_sorskapu.jpg" />
+    <!-- Facebook Like End -->
+ 
+    <title>Sorskapu Hírlevél</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../assets/css/stylish-portfolio.css" rel="stylesheet">
+	
+    <!-- Custom Fonts -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300italic,400italic,600,600italic,700,700italic,900,900italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+   
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+	<!-- Contact Form -->
+		
+</head>
+<!--Google Analytics-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-18292409-16', 'sorskapu.hu');
+  ga('send', 'pageview');
+
+</script>
+<!--//Google Analytics-->
+<body>
+    <!-- Facebook Like -->
+    <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1459644830989485&version=v2.0";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+    </div>
+
+    <!--Menu-->
+	<?php include '../assets/inc/menu.php';?>
+	<!--/Menu-->
+
+    <!--Sorskapu Hírlevél-->
+    <section style="padding:100px 0;">
+	
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+				
+                <div class="panel panel-default">
+                <div class="panel-body">
+                
+<!-- Begin MailChimp Signup Form -->
+
+<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="//sorskapu.us9.list-manage.com/subscribe/post?u=d30b74903cf93470c03f58e04&amp;id=a9f5c76ac0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+    <h2>Iratkozz fel hírlevelünkre</h2>
+<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+    <label for="mce-FNAME">Név  <span class="asterisk">*</span>
+</label>
+    <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+</div>
+<div class="mc-field-group">
+    <label for="mce-EMAIL">Email  <span class="asterisk">*</span>
+</label>
+    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+    <div id="mce-responses" class="clear">
+        <div class="response" id="mce-error-response" style="display:none"></div>
+        <div class="response" id="mce-success-response" style="display:none"></div>
+    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_d30b74903cf93470c03f58e04_a9f5c76ac0" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
+                </div>
+                </div>  
+                
+																								    
+				</div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+		
+    </section>
+	<!--/Sorskapu Hírlevél-->
+		  
+    <!--Footer-->
+	<section id="kapcsolat" style="padding:50px 0;">
+	
+    <footer>
+    <div id="kapcsolat" class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                <br /><br />
+
+                <!--kontakt-->
+                <?php include '../assets/inc/kontakt.php';?>
+                <!--/kontakt-->
+				                
+                </div>
+            </div>
+        </div>
+    </footer>
+	
+	</section>
+    <!--/Footer-->
+	
+    <!-- jQuery Version 1.11.0 -->
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	
+	<!-- Custom Theme JavaScript -->
+    <script>
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('.guru').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    </script>
+	
+</body>
+</html>
