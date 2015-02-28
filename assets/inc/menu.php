5e15f37e2b25e179055d9db77069c5f427c410b4
sorskapu <sorskapu@gmail.com>
2015-02-28 13:45:14 +0100

    update

--- assets/inc/menu.php
+++ assets/inc/menu.php
@@ -59,11 +59,13 @@
 				<li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Melléklet<b class="caret"></b></a>
                         <ul class="dropdown-menu">
-                        <li><a href="/melleklet/a-gondolat-teremto-hatalma/">A gondolat teremtő hatalma</a></li>
-                            <li><a href="/melleklet/biztonsag/">Biztonság</a></li>
-						    <li><a href="/melleklet/kapcsolatok/">Kapcsolatok</a></li>
-							<li><a href="/melleklet/mert-most-tukor-altal-homalyosan-latunk/">Mert most tükör által homályosan látunk</a></li>
-							<!--<li class="divider"></li>-->
+                        <li><a href="/melleklet/gondolatok/">Gondolatok</a></li>
+                        <li class="divider"></li>
+                        <li><a href="/melleklet/biztonsag/">Biztonság</a></li>
+						<li><a href="/melleklet/kapcsolatok/">Kapcsolatok</a></li>
+						<li><a href="/melleklet/mert-most-tukor-altal-homalyosan-latunk/">Mert most tükör által homályosan látunk</a></li>
+						<li><a href="/melleklet/a-gondolat-teremto-hatalma/">A gondolat teremtő hatalma</a></li>
+                        <!--<li class="divider"></li>-->
                         </ul>
                 </li>
                 <li class="dropdown">
