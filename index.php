a5017086dc03cccdeed7429eb034cec8d1e0cb22
sorskapu <sorskapu@gmail.com>
2015-03-06 20:53:06 +0100

    update

--- index.php
+++ index.php
@@ -43,31 +43,16 @@
 
 
 </head>
-<!--Google Analytics-->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-18292409-16', 'sorskapu.hu');
-  ga('send', 'pageview');
-
-</script>
-<!--//Google Analytics-->
+    <!--Before Body-->
+    <?php include 'assets/inc/before-body.php';?>
+    <!--/Before Body-->
 
 <body>
 
-	<!-- Facebook Like -->
-    <div id="fb-root"></div>
-        <script>(function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1459644830989485&version=v2.0";
-            fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
-    </div>
+	<!--After Body-->
+    <?php include 'assets/inc/after-body.php';?>
+    <!--/After Body-->
 
     <!--Menu-->
 	<?php include 'assets/inc/menu.php';?>
