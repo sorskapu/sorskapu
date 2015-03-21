27a520a28616452f5e48be5cee8b6a497596a26b
sorskapu <sorskapu@gmail.com>
2015-03-21 16:05:53 +0100

    update

---
+++ hangjatekok/bert-hellinger-mese-a-duhos-kismajomrol/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Tanmese a dacos, akaratos gyerekeknek és az olykor gyerekesen viselkedő felnőtteknek.">
+	<meta name="keywords" content="Bert Hellinger, mese, tanmese, Kerekes Andrea, Földi Teodóra, düh, akarat, agresszió, családállítás sorskapu">
+	<link rel="shortcut icon" href="http://cdn.sorskapu.hu/sorskapu.ico">
+    
+	
+	<!-- Facebook -->
+	<meta property="fb:app_id" content="1459644830989485" />
+    <meta property="fb:admins" content="100000956925477"/>
+    <meta property="og:url" content="http://www.sorskapu.hu/hangjatekok/bert-hellinger-mese-a-duhos-kismajomrol/"/>
+    <meta property="og:locale" content="hu_HU" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Sorskapu" />
+	<meta property="og:title" content="Bert Hellinger - Mese a dühös kismajomról" />
+    <meta property="og:description" content="Tanmese a dacos, akaratos gyerekeknek és az olykor gyerekesen viselkedő felnőtteknek." />
+    <meta property="og:image" content="http://cdn.sorskapu.hu/fb_hangjatek-002.jpg" />
+    <!-- Facebook Like End -->
+
+    <title>Bert Hellinger - Mese a dühös kismajomról</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../../assets/css/stylish-portfolio.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300italic,400italic,600,600italic,700,700italic,900,900italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+   
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+	<!-- Contact Form -->
+		
+</head>
+
+    <!--Before Body-->
+    <?php include '../../assets/inc/before-body.php';?>
+    <!--/Before Body-->
+
+<body>
+    
+    <!--After Body-->
+    <?php include '../../assets/inc/after-body.php';?>
+    <!--/After Body-->
+
+    <!--Menu-->
+	<?php include '../../assets/inc/menu.php';?>
+	<!--/Menu-->
+
+    <!--Header -->
+    <header id="top" class="header_hangjatek-002">
+        <div class="text-vertical-center">
+            <h4 style="margin-top:100px;" class="btn btn-dark btn-lg">Bert Hellinger - Mese a dühös kismajomról</h4>
+			<div style="clear: both;"></div>
+			<br />
+			<a href="#tovabb" class="btn btn-dark btn-lg guru">Tovább</a><br /><br />
+			<div class="fb-like" data-href="http://www.sorskapu.hu/hangjatekok/bert-hellinger-mese-a-duhos-kismajomrol/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+        </div>
+	</header>
+	<!--/Header-->
+	
+	<!--Bert Hellinger - Mese a dühös kismajomról-->
+	<section id="tovabb" style="padding:50px 0;">
+	
+	<div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+	            <div class="responsive-video"><iframe width="1140" height="641" src="//www.youtube.com/embed/NNAyyWH3EtQ?rel=0" frameborder="0" allowfullscreen></iframe></div>		
+            </div>
+        </div>
+    </div>
+	
+	</section>
+	<!--/Bert Hellinger - Mese a dühös kismajomról-->
+	
+	<!--FB Comments-->
+	    <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	                <div class="fb-comments" data-href="http://www.sorskapu.hu/melleklet/kapcsolatok/" data-width="100%" data-numposts="5" data-colorscheme="light"></div>
+	            </div>
+            </div>																					    
+	    </div>
+    <!--/FB Comments-->
+	
+    <!--Footer-->
+	<section id="kapcsolat" style="padding:50px 0;">
+	
+    <footer>
+    <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                <div class="fb-like" data-href="http://www.sorskapu.hu/hangjatekok/bert-hellinger-mese-a-duhos-kismajomrol/" data-layout="box_count" data-action="like" data-show-faces="true" data-share="true"></div>
+                <br /><br />    
+
+                <!--kontakt-->
+                <?php include '../../assets/inc/kontakt.php';?>
+                <!--/kontakt-->
+
+                </div>
+            </div>
+        </div>
+    </footer>
+	
+	</section>
+	<!--/Footer-->
+    	
+    <!-- jQuery Version 1.11.0 -->
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> 
+	
+    <!-- Custom Theme JavaScript -->
+    <script>
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('.guru').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    </script>
+
+</body>
+</html>
